5006ef0edeeea9c0db6dcde86678c7027d551c96
mohamad <mohamad.fathi13811@gmail.com>
2024-07-22 19:49:12 +0330

    adding transaction to website.models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    transaction_type = [
+        ('expense', 'expense'),
+        ('income', 'income')
+    ]
+    type = models.CharField(choices=transaction_type, max_length=20)
+    description = models.CharField(max_length=255, verbose_name='توضیحات')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ')
+    amount = models.BigIntegerField(verbose_name='مقدار')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+
+    def __str__(self):
+        return '{}-{}-{}'.format(self.date, self.user, self.amount)
+
+    class Meta:
+        verbose_name = 'تراکنش'
+        verbose_name_plural = 'تراکنش ها'
